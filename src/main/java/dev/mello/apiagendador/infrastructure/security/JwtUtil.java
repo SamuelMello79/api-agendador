e87242f5333dd46c6dadbd8e66d670876285a7a5
SamuelMello79 <mello.developer797@gmail.com>
2025-07-27 13:03:48 -0300

    [FIX] Arrumando métodos deprecados na security

--- src/main/java/dev/mello/apiagendador/infrastructure/security/JwtUtil.java
+++ src/main/java/dev/mello/apiagendador/infrastructure/security/JwtUtil.java
@@ -2,27 +2,31 @@ package dev.mello.apiagendador.infrastructure.security;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
-import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 import java.util.Date;
 
 @Service
 public class JwtUtil {
     // Chave secreta usada para assinar e verificar tokens JWT
-    private final String secretKey = "minhas-secrets-keyss-estaos-aqui";
+    private final String secretKey = "bWluaGFzLXNlY3JldHMta2V5c3MtZXN0YW9zLWFxdWk="; // em base64
+
+    private SecretKey getSecretKey() {
+        byte[] key = Base64.getDecoder().decode(secretKey);
+        return Keys.hmacShaKeyFor(key);
+    }
 
     // Extrai as claims do token JWT (informação adicionais)
     public Claims extractClaims(String token) {
         return Jwts.parser()
-                .setSigningKey(Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8)))
+                .verifyWith(getSecretKey()) // muda de signWIth para VerifyWith
                 // Define a chave para validar a assinatura
                 .build()
-                .parseClaimsJws(token) // Analisa o token e obtém as claims
-                .getBody(); // Retorna o corpo das claims
+                .parseSignedClaims(token) // Analisa o token e obtém as claims
+                .getPayload(); // Retorna o corpo das claims
     }
 
     // Extrai o nome de usuário do token JWT
